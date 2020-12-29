1233d1bc6a66b781237334dcdcb72b951e7ced7b
Vadim Zaliva <lord@crocodile.org>
2020-12-29 14:20:00 -0800

    arg parsing

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,71 @@
 module Main where
 
-
+import Control.Monad
 import System.Console.GetOpt
-import Data.Maybe ( fromMaybe )
-       
+import System.Environment(getArgs, getProgName)    
+    
 import Mbox
 
+data Options = Options
+    { optVerbose     :: Bool
+    , optDryRun      :: Bool
+    , optConfig      :: String
+    , optSkip        :: Int
+    , optLimit       :: Maybe Int
+    } deriving Show
+
+defaultOptions = Options
+    { optVerbose     = False
+    , optDryRun      = False
+    , optConfig      = "takeoutupload.cfg"
+    , optSkip        = 0
+    , optLimit       = Nothing
+    }
+    
+options :: [OptDescr (Options -> Either String Options)]
+options =
+    [ Option ['v']     ["verbose"]
+        (NoArg (\ opts -> Right opts { optVerbose = True }))
+        "chatty output on stderr"
+    , Option ['d']     ["dry-run"]
+        (NoArg (\ opts -> Right opts { optDryRun = True }))
+        "verobose progress on stderr"        
+    , Option ['c']     ["config"]
+        (ReqArg (\ d opts -> Right opts { optConfig = d }) "FILE")
+        "config file"
+    , Option ['s'] ["skip"]
+      (ReqArg (\n opts ->
+        case reads n of
+          [(n, "")] | n >= 0 -> Right opts { optSkip = n }
+          _ -> Left "--skip value must be an non-negative integer"
+        ) "number")
+      "number of messages to skip"
+    , Option ['l'] ["limit"]
+      (ReqArg (\n opts ->
+        case reads n of
+          [(n, "")] | n >= 1 -> Right opts { optLimit = Just n }
+          _ -> Left "--limit value must be an positive integer"
+        ) "number")
+      "number of messages to process"
+    ]
+
+parseOpts :: [String] -> IO (Options, String)
+parseOpts argv =
+    do
+      progName <- getProgName
+      let helpMessage = "Usage: " ++ progName ++ " [OPTION...] <inputfile>"
+      case getOpt Permute options argv of
+        (o,[n],[]) -> 
+            case foldM (flip id) defaultOptions o of
+              Right opts -> return (opts,n)
+              Left errorMessage -> ioError (userError (errorMessage ++ "\n" ++ helpMessage))
+        (_,_,errs) -> ioError (userError (concat errs ++ usageInfo helpMessage options))
+
+
 main :: IO ()
-main = someFunc
+main =
+    do
+    (opts, inputfile) <- getArgs >>= parseOpts
+    ; putStr inputfile
+    ; putStr (show opts)
+    someFunc
